47f3b8b0a5f392ff5b1a8b2d32811e9a51f5cbd8
Vanessa <vanessalvarez8a@gmail.com>
2015-05-09 23:17:22 +0000

    Added X:XX Total Length

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -11,16 +11,26 @@
       + '      <br/>'
       + '      X songs'
       + '      <br/>'
+      + '      "X:XX Total Length"'
+      + '       </br>'
       + '    </p>'
       + '  </div>'
       + '</div>';
  
    return $(template);
  };
- 
+
+ //This code will throw a random number between 25 and 100 albums
+ // var x = Math.random() * 100 + 25;
+ //for(var i = 0; i < x; i++) {
+// var $newThumbnail = buildAlbumThumbnail();
+// $collection.append($newThumbnail);
+// }
+
+// *********************** 
+
  // THIS VARIABLE EMPTY ANYTHING THAT IS INSIDE .collection-container and .row 
  // AND IT WOULD REPEAT 36 TIMES THE OVERWRITTEN INFO THAT IS NOW A JQUERY OBJECT
-
 var updateCollectionView = function() {
    var $collection = $('.collection-container .row');
    $collection.empty();
@@ -31,9 +41,10 @@ var updateCollectionView = function() {
     }
 };
 
+
 if(document.URL.match(/\/collection.html/)) {
   // Wait until the HTML is fully processed.
-  $(document).ready(function() {
+$(document).ready(function() {
   // WHEN THE PAGE LOADS THE CODE WILL EXECUTE 
     updateCollectionView();
     
