3bec109126b3b1a3eddda94dfe6832cca6b26f93
Vanessa <vanessalvarez8a@gmail.com>
2015-06-09 01:27:56 +0000

    Converted landing page to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -265,10 +265,42 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require('./collection');
-require('./album');
-require('./profile');
+// require("./landing");
+// require('./collection');
+// require('./album');
+// require('./profile');
+
+ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.mainTitle = "Bloc Jams";  
+  $scope.subText = "Turn the music up!";
+
+
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
+
+    $scope.mainTitleClicked = function() {
+    function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+      shuffle($scope.albumURLs);
+    };
+
+
+  $scope.albumURLs = [
+   '/images/album-placeholders/album-1.jpg',
+   '/images/album-placeholders/album-2.jpg',
+   '/images/album-placeholders/album-3.jpg',
+   '/images/album-placeholders/album-4.jpg',
+   '/images/album-placeholders/album-5.jpg',
+   '/images/album-placeholders/album-6.jpg',
+   '/images/album-placeholders/album-7.jpg',
+   '/images/album-placeholders/album-8.jpg',
+   '/images/album-placeholders/album-9.jpg',
+  ];
+
+ }]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
