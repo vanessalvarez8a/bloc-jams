889d1b25737e0d46dac26bccc9e176198f010632
Vanessa <vanessalvarez8a@gmail.com>
2015-07-04 15:21:49 +0000

    Converted album view to Angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -302,6 +302,12 @@ var albumPicasso = {
      url: '/collection',
      controller: 'Collection.controller',
      templateUrl: '/templates/collection.html'
+  })
+
+  $stateProvider.state('album', {
+    url: '/album',
+    controller: 'Album.controller',
+    templateUrl: '/templates/album.html'
   });
 
  }]);
@@ -346,6 +352,44 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
    }
 }]);
 
+ blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+
+  var hoveredSong = null;
+  var playingSong = null;
+
+
+  // onHover offHover functionality 
+   $scope.onHoverSong = function(song) {
+     hoveredSong = song;
+   };
+ 
+   $scope.offHoverSong = function(song) {
+     hoveredSong = null;
+   };
+
+  // playing-pause functionality  
+  $scope.playSong = function(song) {
+    playingSong = song;
+  };
+
+  $scope.pauseSong = function(song) {
+    playingSong = null;
+  };
+
+  // state functionality
+
+   $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+   };
+
+ }]);
 
 
 
