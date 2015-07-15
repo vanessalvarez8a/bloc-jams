69ba25a1bacb174a786515efa81630663c8b30bc
Vanessa <vanessalvarez8a@gmail.com>
2015-07-15 23:14:55 +0000

    Added volume control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -145,6 +145,15 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
 
+
+  $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+  }
+
    SongPlayer.onTimeUpdate(function(event, time){
      $scope.$apply(function(){
        $scope.playTime = time;
@@ -162,6 +171,7 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
      currentSong: null,
      currentAlbum: null,
      playing: false,
+     volume: 90,
  
      play: function() {
        this.playing = true;
@@ -199,6 +209,13 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
        }
      },
 
+    setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
+
      onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
      },
@@ -214,6 +231,7 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
         formats: [ "mp3" ],
         preload: true
     });
+      currentSoundFile.setVolume(this.volume);
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
       });
